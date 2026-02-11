5e09ab5019231bb2276484b7822d7f3d159a084a
alguemchamadoeverton <vrtnrdrgs@gmail.com>
2026-02-10 22:59:04 -0300

    Criando a entidade equipment

---
+++ MinhaPrimeiraAPI/Models/Equipment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinhaPrimeiraAPI.Models
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public List<Exercise> Exercises = new List<Exercise>();
+    }
+}
